854569a7cd3829472bf8a6aea64574bb4dd61f07
JoseMiguel <josemiguel155@gmail.com>
2022-12-12 20:56:21 -0500

    validaciones de campos en formulario

--- src/app/components/formularios/formulario-reactivos/formulario-reactivos.component.ts
+++ src/app/components/formularios/formulario-reactivos/formulario-reactivos.component.ts
@@ -1,41 +1,70 @@
-import { Component ,OnInit,OnDestroy } from '@angular/core';
-import {FormGroup,FormBuilder} from '@angular/forms';
+import { Component,OnInit,OnDestroy } from '@angular/core';
+import { FormGroup,FormBuilder,Validators,AbstractControl } from "@angular/forms";
+
 
 @Component({
   selector: 'app-formulario-reactivos',
   templateUrl: './formulario-reactivos.component.html',
   styleUrls: ['./formulario-reactivos.component.css']
 })
-export class FormularioReactivosComponent implements OnInit, OnDestroy {
-  //variable que represnta el formulario
-formGroup :FormGroup = new FormGroup({});
-//se inyecta clase
-constructor(private formBuilder:FormBuilder){
+export class FormularioReactivosComponent implements OnInit, OnDestroy{
 
-}
-//se ejecuta en el init del formulario
-ngOnInit(): void {
-  //debugger;
-  console.log('Componente init');
-  this.builForm();
-}
-//se ejecuta cundo se destruya el componente
-ngOnDestroy(): void {
-  console.log('Componente DESTRUIDO');
-}
-//variable para construir el formulario
-builForm(){
-  //se crean los controles cada input es un control
-  this.formGroup=this.formBuilder.group({
-   nombre:[''],
-   apellido:[''],
-   correo:[''],
-   contrasena:['']
-  });
-}
+  formGroup: FormGroup = new FormGroup({});
 
-guardar()
-{
-  console.log(this.formGroup);
-}
+  constructor(private formBuilder: FormBuilder){
+    
+  }
+
+  ngOnInit(): void {
+    console.log('Componente init');
+    this.buildForm();
+  }
+
+  ngOnDestroy(): void {
+    
+    console.log('Componente DESTRUIDO');
+  }
+
+  buildForm(){
+    this.formGroup = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      apellido: ['', [Validators.required, Validators.minLength(5)]],
+      correo: ['', Validators.pattern('[a-z0-9]+@[a-z0-9]+\.[a-z]{2,4}$')],
+      contrasena: ['', [Validators.required, this.validarContrasena]]
+    });
+  }
+
+  validarContrasena(control:AbstractControl){
+    const contrasena:string = control.value;
+    let error = null;
+    if(!contrasena.includes('$')){
+      error = { pesos: 'Es requerido el signo $' }
+    }
+    if(!parseFloat(contrasena[0])){
+      error = { ...error, number: 'Es requerido empezar con un numero' }
+    }
+    return error;
+  }
+
+  get usuarioNoValido(){
+    return this.nombre?.invalid && this.nombre.touched
+  }
+
+  getError(controlName: string){
+    let error = '';
+    const control = this.formGroup.get(controlName);
+    if(control?.touched && control?.errors != null ){
+      error = JSON.stringify(control?.errors)
+    }
+    return error;
+  }
+
+
+  guardar(){
+    console.log(this.formGroup);
+  }
+
+  get nombre(): AbstractControl | null { return this.formGroup.get('nombre') };
+  get apellido(): AbstractControl | null { return this.formGroup.get('apellido') };
+  get contrasena(): AbstractControl | null { return this.formGroup.get('contrasena') };
 }
